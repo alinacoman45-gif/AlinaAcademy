0bc205b4a2d2c5763b05b1929496272d790316ab
alinacoman45-gif <alinacoman45@icloud.com>
2025-12-04 18:42:45 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Benefits from "@/components/Benefits";
+import Benefits from "../components/Benefits";
 
 export default function Home() {
   return (
